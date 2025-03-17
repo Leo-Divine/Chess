d6054668297def06bd5e7ee7cf26ddade140968d
Leo <64170386+Leo-Divine@users.noreply.github.com>
2025-03-17 08:26:15 -0400

    Update grid.rs
    Used Index and IndexMut instead of a get_index function. Will have to check if it works later, formatted the code, but clippy is yelling at me, so I'll check it when I get back to a computer.

--- src/grid.rs
+++ src/grid.rs
@@ -24,62 +24,58 @@ pub enum Pieces {
 
 #[derive(Default)]
 pub struct Grid {
-  rows: usize,
-  cols: usize,
-  items: Vec<Pieces>,
+    rows: usize,
+    cols: usize,
+    items: Vec<Pieces>,
 }
 
 impl Grid {
-  pub fn new() -> Self {
-      Self {
-          rows: ROWS,
-          cols: COLS,
-          items: vec![Pieces::None; ROWS * COLS],
-      }
-  }
+    pub fn new() -> Self {
+        Self {
+            rows: ROWS,
+            cols: COLS,
+            items: vec![Pieces::None; ROWS * COLS],
+        }
+    }
 
-  pub fn add_starting_pieces(&mut self) {
-      for x in 0..8 {
-          self.items[Self::get_index(x, 1)] = Pieces::BlackPawn;
-          self.items[Self::get_index(x, 6)] = Pieces::WhitePawn;
-      }
+    fn add_starting_pieces(&mut self) {
+        for x in 0..8 {
+            self.items[(x, 1)] = Pieces::BlackPawn;
+            self.items[(x, 6)] = Pieces::WhitePawn;
+        }
 
-      self.items[Self::get_index(0, 0)] = Pieces::BlackRook;
-      self.items[Self::get_index(1, 0)] = Pieces::BlackKnight;
-      self.items[Self::get_index(2, 0)] = Pieces::BlackBishop;
-      self.items[Self::get_index(3, 0)] = Pieces::BlackKing;
-      self.items[Self::get_index(4, 0)] = Pieces::BlackQueen;
-      self.items[Self::get_index(5, 0)] = Pieces::BlackBishop;
-      self.items[Self::get_index(6, 0)] = Pieces::BlackKnight;
-      self.items[Self::get_index(7, 0)] = Pieces::BlackRook;
+        self.items[(0, 0)] = Pieces::BlackRook;
+        self.items[(1, 0)] = Pieces::BlackKnight;
+        self.items[(2, 0)] = Pieces::BlackBishop;
+        self.items[(3, 0)] = Pieces::BlackKing;
+        self.items[(4, 0)] = Pieces::BlackQueen;
+        self.items[(5, 0)] = Pieces::BlackBishop;
+        self.items[(6, 0)] = Pieces::BlackKnight;
+        self.items[(7, 0)] = Pieces::BlackRook;
 
-      self.items[Self::get_index(0, 7)] = Pieces::WhiteRook;
-      self.items[Self::get_index(1, 7)] = Pieces::WhiteKnight;
-      self.items[Self::get_index(2, 7)] = Pieces::WhiteBishop;
-      self.items[Self::get_index(3, 7)] = Pieces::WhiteKing;
-      self.items[Self::get_index(4, 7)] = Pieces::WhiteQueen;
-      self.items[Self::get_index(5, 7)] = Pieces::WhiteBishop;
-      self.items[Self::get_index(6, 7)] = Pieces::WhiteKnight;
-      self.items[Self::get_index(7, 7)] = Pieces::WhiteRook;
-  }
-
-  fn get_index(col: usize, row: usize) -> usize {
-      ROWS * row + col
-  }
+        self.items[(0, 7)] = Pieces::WhiteRook;
+        self.items[(1, 7)] = Pieces::WhiteKnight;
+        self.items[(2, 7)] = Pieces::WhiteBishop;
+        self.items[(3, 7)] = Pieces::WhiteKing;
+        self.items[(4, 7)] = Pieces::WhiteQueen;
+        self.items[(5, 7)] = Pieces::WhiteBishop;
+        self.items[(6, 7)] = Pieces::WhiteKnight;
+        self.items[(7, 7)] = Pieces::WhiteRook;
+    }
 }
 
 impl Index<(usize, usize)> for Grid {
-  type Output = Pieces;
+    type Output = Pieces;
 
-  fn index(&self, (col, row): (usize, usize)) -> &Self::Output {
-      &self.items[self.rows * row + col]
-  }
+    fn index(&self, (col, row): (usize, usize)) -> &Self::Output {
+        &self.items[self.rows * row + col]
+    }
 }
 
 impl IndexMut<(usize, usize)> for Grid {
-  fn index_mut(&mut self, (col, row): (usize, usize)) -> &mut Self::Output {
-      &mut self.items[self.rows * row + col]
-  }
+    fn index_mut(&mut self, (col, row): (usize, usize)) -> &mut Self::Output {
+        &mut self.items[self.rows * row + col]
+    }
 }
 
 pub type Board = Arc<Mutex<Grid>>;
