cc5ef9ee9f91cc61bbf3ef482fe7b613d1e8c2d2
Leo <64170386+Leo-Divine@users.noreply.github.com>
2025-03-17 09:46:26 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,7 @@ mod grid;
 
 fn main() {
     let grid: Board = Arc::new(Mutex::new(Grid::new()));
-    grid.lock().unwrap().add_starting_pieces();
-    /* 
-    println!("{:?}", board.get_piece_at(0, 3));
-    board.test_piece_move();
-    println!("{:?}", board.get_piece_at(0, 3));
-    */
+    
     let ui:UI = UI::new(Arc::clone(&grid));
     assert_eq!(ui.board.lock().unwrap()[(0, 0)], grid.lock().unwrap()[(0, 0)]);
     iced::run("Chess", UI::update, UI::view);
