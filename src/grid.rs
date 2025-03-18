79cf0b1724bc8511203e888585c4bf98d3cb2915
Leo <64170386+Leo-Divine@users.noreply.github.com>
2025-03-18 14:42:28 -0400

    Update grid.rs

--- src/grid.rs
+++ src/grid.rs
@@ -28,31 +28,17 @@ pub struct Grid {
 
 impl Default for Grid {
     fn default() -> Self {
-        let mut items: Vec<Pieces> = vec![Pieces::None; ROWS * COLS];
-        for x in 0..8 {
-            items[Grid::get_index(x, 1)] = Pieces::BlackPawn;
-            items[Grid::get_index(x, 6)] = Pieces::WhitePawn;
+        Self { 
+            vec![Pieces::BlackRook, Pieces::BlackKnight, Pieces::BlackBishop, Pieces::BlackKing, Pieces::BlackQueen, Pieces::BlackBishop, Pieces::BlackKnight, Pieces::BlackRook,
+                Pieces::BlackPawn, Pieces::BlackPawn, Pieces::BlackPawn, Pieces::BlackPawn, Pieces::BlackPawn, Pieces::BlackPawn, Pieces::BlackPawn, Pieces::BlackPawn,
+                Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None,
+                Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None,
+                Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None,
+                Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None, Pieces::None,
+                Pieces::WhitePawn, Pieces::WhitePawn, Pieces::WhitePawn, Pieces::WhitePawn, Pieces::WhitePawn, Pieces::WhitePawn, Pieces::WhitePawn, Pieces::WhitePawn,
+                Pieces::WhiteRook, Pieces::WhiteKnight, Pieces::WhiteBishop, Pieces::WhiteKing, Pieces::WhiteQueen, Pieces::WhiteBishop, Pieces::WhiteKnight, Pieces::WhiteRook
+            ]
         }
-
-        items[Grid::get_index(0, 0)] = Pieces::BlackRook;
-        items[Grid::get_index(1, 0)] = Pieces::BlackKnight;
-        items[Grid::get_index(2, 0)] = Pieces::BlackBishop;
-        items[Grid::get_index(3, 0)] = Pieces::BlackKing;
-        items[Grid::get_index(4, 0)] = Pieces::BlackQueen;
-        items[Grid::get_index(5, 0)] = Pieces::BlackBishop;
-        items[Grid::get_index(6, 0)] = Pieces::BlackKnight;
-        items[Grid::get_index(7, 0)] = Pieces::BlackRook;
-
-        items[Grid::get_index(0, 7)] = Pieces::WhiteRook;
-        items[Grid::get_index(1, 7)] = Pieces::WhiteKnight;
-        items[Grid::get_index(2, 7)] = Pieces::WhiteBishop;
-        items[Grid::get_index(3, 7)] = Pieces::WhiteKing;
-        items[Grid::get_index(4, 7)] = Pieces::WhiteQueen;
-        items[Grid::get_index(5, 7)] = Pieces::WhiteBishop;
-        items[Grid::get_index(6, 7)] = Pieces::WhiteKnight;
-        items[Grid::get_index(7, 7)] = Pieces::WhiteRook;
-
-        Self { items }
     }
 }
 
