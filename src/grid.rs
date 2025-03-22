57600b6a3e747e42e19f62142481a3efad2c6ad7
booyaalex <you@example.com>
2025-03-22 11:04:32 -0400

    Pieces now move properly, turns are implemented, still have to get pawn captures working

--- src/grid.rs
+++ src/grid.rs
@@ -21,6 +21,7 @@ pub enum Pieces {
 
 pub struct Grid {
     items: Vec<Pieces>,
+    turn: bool, //true = white; false = black;
 }
 
 impl Default for Grid {
@@ -92,17 +93,173 @@ impl Default for Grid {
                 Pieces::WhiteKnight,
                 Pieces::WhiteRook,
             ],
+            turn: true,
         }
     }
 }
 
 impl Grid {
     pub fn move_piece(&mut self, old_position: (usize, usize), new_position: (usize, usize)) {
+        let white_pieces: [Pieces; 6] = [
+            Pieces::WhitePawn,
+            Pieces::WhiteKnight,
+            Pieces::WhiteBishop,
+            Pieces::WhiteRook,
+            Pieces::WhiteQueen,
+            Pieces::WhiteKing,
+        ];
+        let black_pieces: [Pieces; 6] = [
+            Pieces::BlackPawn,
+            Pieces::BlackKnight,
+            Pieces::BlackBishop,
+            Pieces::BlackRook,
+            Pieces::BlackQueen,
+            Pieces::BlackKing,
+        ];
+
+        //Don't move nothing
         if self[old_position] == Pieces::None {
             return;
         }
+        //Make sure a piece moved
+        if old_position == new_position {
+            return;
+        }
+        //It better be your turn
+        if (self.turn && black_pieces.contains(&self[old_position]))
+            || (!self.turn && white_pieces.contains(&self[old_position]))
+        {
+            return;
+        }
+        //Make sure the piece moved correctly
+        if !self.is_move_valid(self[old_position].clone(), old_position, new_position) {
+            return;
+        }
+        //Don't capture your own pieces
+        if (self.turn && white_pieces.contains(&self[new_position]))
+            || (!self.turn && black_pieces.contains(&self[new_position]))
+        {
+            return;
+        }
+
         self[new_position] = self[old_position].clone();
         self[old_position] = Pieces::None;
+        self.turn = !self.turn;
+    }
+    fn is_move_valid(
+        &mut self,
+        piece: Pieces,
+        old_position: (usize, usize),
+        new_position: (usize, usize),
+    ) -> bool {
+        let x_move: i8 = (new_position.0 as i8) - (old_position.0 as i8);
+        let y_move: i8 = (new_position.1 as i8) - (old_position.1 as i8);
+        let x_abs: i8 = x_move.abs();
+        let y_abs: i8 = y_move.abs();
+
+        //BLACK PAWN
+        if piece == Pieces::BlackPawn {
+            if y_move != 1 || x_move != 0 {
+                if old_position.1 != 1 || y_move != 2 {
+                    return false;
+                }
+            }
+        }
+        //WHITE PAWN
+        if piece == Pieces::WhitePawn {
+            if y_move != -1 || x_move != 0 {
+                if old_position.1 != 6 || y_move != -2 {
+                    return false;
+                }
+            }
+        }
+        //ROOK
+        if piece == Pieces::BlackRook || piece == Pieces::WhiteRook {
+            if old_position.0 != new_position.0 && old_position.1 != new_position.1 {
+                return false;
+            }
+            if self.is_jumping_piece(piece.clone(), old_position, x_move, y_move) {
+                return false;
+            }
+        }
+        //KNIGHT
+        if piece == Pieces::BlackKnight || piece == Pieces::WhiteKnight {
+            if (x_abs != 2 || y_abs != 1) && (x_abs != 1 || y_abs != 2) {
+                return false;
+            }
+        }
+        //BISHOP
+        if piece == Pieces::BlackBishop || piece == Pieces::WhiteBishop {
+            if (x_move + y_move) != 0i8 && (x_move - y_move) != 0i8 {
+                return false;
+            }
+        }
+        //QUEEN
+        if piece == Pieces::BlackQueen || piece == Pieces::WhiteQueen {
+            if ((x_move + y_move) != 0i8 && (x_move - y_move) != 0i8)
+                && (old_position.0 != new_position.0 && old_position.1 != new_position.1)
+            {
+                return false;
+            }
+        }
+        //KING
+        if piece == Pieces::BlackKing || piece == Pieces::WhiteKing {
+            if (x_abs != 0 || y_abs != 1)
+                && (x_abs != 1 || y_abs != 0)
+                && (x_abs != 1 || y_abs != 1)
+            {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn is_jumping_piece(
+        &mut self,
+        piece: Pieces,
+        old_position: (usize, usize),
+        x_move: i8,
+        y_move: i8,
+    ) -> bool {
+        fn check_horizontally(grid: &mut Grid, old_position: (usize, usize), x_move: i8) -> bool {
+            for i in 1..x_move {
+                if grid[((old_position.0 as i8 + i) as usize, old_position.1)] != Pieces::None {
+                    return true;
+                }
+            }
+            for i in x_move..0 {
+                if i == x_move {
+                    continue;
+                }
+                if grid[((old_position.0 as i8 + i) as usize, old_position.1)] != Pieces::None {
+                    return true;
+                }
+            }
+            false
+        }
+        fn check_vertically(grid: &mut Grid, old_position: (usize, usize), y_move: i8) -> bool {
+            for i in 1..y_move {
+                if grid[(old_position.0, (old_position.1 as i8 + i) as usize)] != Pieces::None {
+                    return true;
+                }
+            }
+            for i in y_move..0 {
+                if i == y_move {
+                    continue;
+                }
+                if grid[(old_position.0, (old_position.1 as i8 + i) as usize)] != Pieces::None {
+                    return true;
+                }
+            }
+            false
+        }
+        if piece == Pieces::BlackRook || piece == Pieces::WhiteRook {
+            if x_move == 0 {
+                return check_vertically(self, old_position, y_move);
+            }
+            return check_horizontally(self, old_position, x_move);
+        }
+        false
     }
 }
 
