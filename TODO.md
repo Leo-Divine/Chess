233bf3ed2e5fdf6e7c65bac925ed8f33d2d54a1e
Leo <isidororossinia@gmail.com>
2025-03-24 10:54:42 -0400

    Fixed bug with checking diagonal jumping, changed the pieces vec to an array, fixed pawn movement, you can't put a king into check

--- TODO.md
+++ TODO.md
@@ -1,9 +1,19 @@
 ## Todo
 ### Grid
 - [x] Figure out Arc & Mutex
-- [ ] Replace the Pieces Vec with an Array
+- [x] Replace the Pieces Vec with an Array
 - [X] Finish Piece Jumping
+- [x] Add Proper Pawn Movement
+    - [X] Pawn Capturing
+    - [x] Prevent Forward Pawn Captures
+- [ ] Add Checks and Checkmates
+    - [x] Checks
+    - [ ] Checkmates
+- [ ] Change Pieces to Piece Struct
 ### Main
 - [x] Add results for main.rs
 ### UI
 - [X] Add a Proper Theme for the Board
+- [ ] Add a Previous Move Log
+- [ ] Add Some Text for Player Turn and Game Status
+- [ ] Properly Scale Pieces to Different Screen Sizes
--- src/grid.rs
+++ src/grid.rs
@@ -1,6 +1,11 @@
 use std::ops::{Index, IndexMut};
 use std::sync::{Arc, Mutex};
 
+struct Piece {
+    piece_type: Pieces,
+    position: (usize, usize),
+}
+
 #[derive(Debug, Default, Clone, PartialEq)]
 pub enum Pieces {
     #[default]
@@ -20,14 +25,14 @@ pub enum Pieces {
 }
 
 pub struct Grid {
-    items: Vec<Pieces>,
+    items: [Pieces; 64],
     turn: bool, //true = white; false = black;
 }
 
 impl Default for Grid {
     fn default() -> Self {
         Self {
-            items: vec![
+            items: [
                 Pieces::BlackRook,
                 Pieces::BlackKnight,
                 Pieces::BlackBishop,
@@ -141,13 +146,108 @@ impl Grid {
         {
             return;
         }
-
+        //Don't put yourself in check idiot
+        let temp_old_piece = self[old_position].clone();
+        let temp_new_piece = self[new_position].clone();
         self[new_position] = self[old_position].clone();
         self[old_position] = Pieces::None;
+        if self.in_check(!self.turn) {
+            self[old_position] = temp_old_piece;
+            self[new_position] = temp_new_piece;
+            return;
+        }
+
+        //Check for checks
+        if self.in_check(self.turn) {
+            println!("Holy fuck you're dead");
+        }
+
         self.turn = !self.turn;
     }
+    fn get_white_pieces(&self) -> Vec<Piece> {
+        let mut pieces: Vec<Piece> = Vec::new();
+        let white_pieces: [Pieces; 6] = [
+            Pieces::WhitePawn,
+            Pieces::WhiteKnight,
+            Pieces::WhiteBishop,
+            Pieces::WhiteRook,
+            Pieces::WhiteQueen,
+            Pieces::WhiteKing,
+        ];
+
+        for i in 0..self.items.len() {
+            let x: usize = i % 8;
+            let y: usize = i / 8;
+            let piece = self[(x, y)].clone();
+            if white_pieces.contains(&piece) {
+                pieces.push(Piece {
+                    piece_type: piece,
+                    position: (x, y),
+                });
+            }
+        }
+        pieces
+    }
+    fn get_black_pieces(&self) -> Vec<Piece> {
+        let mut pieces: Vec<Piece> = Vec::new();
+        let black_pieces: [Pieces; 6] = [
+            Pieces::BlackPawn,
+            Pieces::BlackKnight,
+            Pieces::BlackBishop,
+            Pieces::BlackRook,
+            Pieces::BlackQueen,
+            Pieces::BlackKing,
+        ];
+
+        for i in 0..self.items.len() {
+            let x: usize = i % 8;
+            let y: usize = i / 8;
+            let piece = self[(x, y)].clone();
+            if black_pieces.contains(&piece) {
+                pieces.push(Piece {
+                    piece_type: piece,
+                    position: (x, y),
+                });
+            }
+        }
+        pieces
+    }
+    fn get_white_king(&self) -> Piece {
+        for i in 0..self.items.len() {
+            let x: usize = i % 8;
+            let y: usize = i / 8;
+            let piece = self[(x, y)].clone();
+            if piece == Pieces::WhiteKing {
+                return Piece {
+                    piece_type: Pieces::WhiteKing,
+                    position: (x, y),
+                };
+            }
+        }
+        Piece {
+            piece_type: Pieces::None,
+            position: (0, 0),
+        }
+    }
+    fn get_black_king(&self) -> Piece {
+        for i in 0..self.items.len() {
+            let x: usize = i % 8;
+            let y: usize = i / 8;
+            let piece = self[(x, y)].clone();
+            if piece == Pieces::BlackKing {
+                return Piece {
+                    piece_type: Pieces::BlackKing,
+                    position: (x, y),
+                };
+            }
+        }
+        Piece {
+            piece_type: Pieces::None,
+            position: (0, 0),
+        }
+    }
     fn is_move_valid(
-        &mut self,
+        &self,
         piece: Pieces,
         old_position: (usize, usize),
         new_position: (usize, usize),
@@ -159,16 +259,38 @@ impl Grid {
 
         //BLACK PAWN
         if (piece == Pieces::BlackPawn)
-            && (y_move != 1 || x_move != 0)
-            && (old_position.1 != 1 || y_move != 2)
+            && (y_move != 1
+                || x_move != 0
+                || self[(new_position.0, new_position.1)] != Pieces::None)
+            && (old_position.1 != 1
+                || y_move != 2
+                || x_move != 0
+                || self[(new_position.0, new_position.1)] != Pieces::None)
         {
+            if y_move == 1
+                && (x_move == -1 || x_move == 1)
+                && (self[(new_position.0, new_position.1)] != Pieces::None)
+            {
+                return true;
+            }
             return false;
         }
         //WHITE PAWN
         if (piece == Pieces::WhitePawn)
-            && (y_move != -1 || x_move != 0)
-            && (old_position.1 != 6 || y_move != -2)
+            && (y_move != -1
+                || x_move != 0
+                || self[(new_position.0, new_position.1)] != Pieces::None)
+            && (old_position.1 != 6
+                || y_move != -2
+                || x_move != 0
+                || self[(new_position.0, new_position.1)] != Pieces::None)
         {
+            if y_move == -1
+                && (x_move == -1 || x_move == 1)
+                && (self[(new_position.0, new_position.1)] != Pieces::None)
+            {
+                return true;
+            }
             return false;
         }
         //ROOK
@@ -211,13 +333,13 @@ impl Grid {
     }
 
     fn is_jumping_piece(
-        &mut self,
+        &self,
         piece: Pieces,
         old_position: (usize, usize),
         x_move: i8,
         y_move: i8,
     ) -> bool {
-        fn check_horizontally(grid: &mut Grid, old_position: (usize, usize), x_move: i8) -> bool {
+        fn check_horizontally(grid: &Grid, old_position: (usize, usize), x_move: i8) -> bool {
             for i in 1..x_move {
                 if grid[((old_position.0 as i8 + i) as usize, old_position.1)] != Pieces::None {
                     return true;
@@ -233,7 +355,7 @@ impl Grid {
             }
             false
         }
-        fn check_vertically(grid: &mut Grid, old_position: (usize, usize), y_move: i8) -> bool {
+        fn check_vertically(grid: &Grid, old_position: (usize, usize), y_move: i8) -> bool {
             for i in 1..y_move {
                 if grid[(old_position.0, (old_position.1 as i8 + i) as usize)] != Pieces::None {
                     return true;
@@ -249,11 +371,11 @@ impl Grid {
             }
             false
         }
-        fn check_diagonally_pos(grid: &mut Grid, old_position: (usize, usize), x_move: i8) -> bool {
+        fn check_diagonally_pos(grid: &Grid, old_position: (usize, usize), x_move: i8) -> bool {
             for i in 1..x_move {
                 if grid[(
-                    (old_position.0 as i8 - i) as usize,
-                    (old_position.1 as i8 + i) as usize,
+                    (old_position.0 as i8 + i) as usize,
+                    (old_position.1 as i8 - i) as usize,
                 )] != Pieces::None
                 {
                     return true;
@@ -273,7 +395,7 @@ impl Grid {
             }
             false
         }
-        fn check_diagonally_neg(grid: &mut Grid, old_position: (usize, usize), x_move: i8) -> bool {
+        fn check_diagonally_neg(grid: &Grid, old_position: (usize, usize), x_move: i8) -> bool {
             for i in 1..x_move {
                 if grid[(
                     (old_position.0 as i8 + i) as usize,
@@ -329,6 +451,24 @@ impl Grid {
         //KING
         if piece == Pieces::BlackKing || piece == Pieces::WhiteKing {
             return check_horizontally(self, old_position, y_move);
+        }
+        false
+    }
+    fn in_check(&self, attacker: bool) -> bool {
+        let attacking_pieces: Vec<Piece>;
+        let defending_king: Piece;
+        if attacker {
+            attacking_pieces = self.get_white_pieces();
+            defending_king = self.get_black_king();
+        } else {
+            attacking_pieces = self.get_black_pieces();
+            defending_king = self.get_white_king();
+        }
+
+        for piece in attacking_pieces {
+            if self.is_move_valid(piece.piece_type, piece.position, defending_king.position) {
+                return true;
+            }
         }
         false
     }
